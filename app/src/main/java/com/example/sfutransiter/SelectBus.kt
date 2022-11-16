cf702779307f3226c713a537e7db565f16e8c5f2
seoluke7203 <seoluke7203@hotmail.com>
2022-11-15 22:40:09 -0500

    [Nov.15] Added Simple UI stuffs
    main.xml
    search_by.xml
    select_bus.xml
    select_station.xml
    bus_summary.xml
    comment_board.xml

---
+++ app/src/main/java/com/example/sfutransiter/SelectBus.kt
@@ -0,0 +1,11 @@
+package com.example.sfutransiter
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SelectBus : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_select_bus)
+    }
+}
